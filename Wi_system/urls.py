ec722957c89436ba3a8a57fd17664dc40d7668fa
Jiajun Chen <31739437+jiajunc@users.noreply.github.com>
2018-10-11 20:16:49 -0500

    Update urls.py

--- Wi_system/urls.py
+++ Wi_system/urls.py
@@ -20,6 +20,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('/index', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('/index.html', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('django.contrib.auth.urls')),
